3673d50635596c2f3d3bde32afc60d19156054cb
roberto <robpalacios1@hotmail.com>
2020-08-31 18:46:26 -0500

    add Hooks useRef, useEffect, useState

--- src/Switch.js
+++ src/Switch.js
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, {useRef, useEffect, useState} from 'react'
 import './Switch.css'
 
 function Switch() {
+    const [checked, setChecked] = useState(false)
+    const ref = useRef(null)
+    function handleChange(){
+        console.log(ref.current.checked)
+        setChecked(ref.current.checked)
+        if (ref.current.checked) {
+            document.body.classList.remove('is-light-mode')
+            document.body.classList.add('is-dark-mode')
+        } else {
+            document.body.classList.remove('is-dark-mode')
+            document.body.classList.add('is-light-mode')
+        }
+    }
+
+    useEffect(() => {
+        if(window.matchMedia('(prefers-color-scheme:dark)').matches) {
+            setChecked(true)
+        }
+    }, [])
+
+
     return (
         <div className="dark-mode">
             <p className="dark-mode-title">Dark Mode</p>
-            <input type="checkbox" className="checkbox" id="checkbox" />
-            <label className="switch" htmlfor="checkbox">
+            <input ref={ref} onChange={handleChange} type="checkbox" checked={checked} className="checkbox" id="checkbox" />
+            <label className="switch" htmlFor="checkbox">
 
             </label>
         </div>
