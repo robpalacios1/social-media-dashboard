b8701fd96810a6d1a571baeee3d2addaaf176171
roberto <robpalacios1@hotmail.com>
2020-08-31 12:58:23 -0500

    add component to render

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react';
 import Header from './Header'
 import TopCardList from './Top-card-list'
+import Overview from './Overview'
 import './Global.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <Fragment>
       <Header />
       <TopCardList />
+      <Overview />
     </Fragment>
   );
 }
