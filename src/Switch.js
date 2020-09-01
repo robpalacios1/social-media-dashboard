ab84930b7ad6632f133b9f02e9154fe8769ed59a
roberto <robpalacios1@hotmail.com>
2020-09-01 00:03:45 -0500

    add paremeters into Switch function

--- src/Switch.js
+++ src/Switch.js
@@ -1,27 +1,13 @@
-import React, {useRef, useEffect, useState} from 'react'
+import React, { useRef } from 'react'
 import './Switch.css'
 
-function Switch() {
-    const [checked, setChecked] = useState(false)
+function Switch({ setDarkMode, checked, setChecked }) {
+
     const ref = useRef(null)
     function handleChange(){
-        console.log(ref.current.checked)
         setChecked(ref.current.checked)
-        if (ref.current.checked) {
-            document.body.classList.remove('is-light-mode')
-            document.body.classList.add('is-dark-mode')
-        } else {
-            document.body.classList.remove('is-dark-mode')
-            document.body.classList.add('is-light-mode')
-        }
+        setDarkMode(ref.current.checked)
     }
-
-    useEffect(() => {
-        if(window.matchMedia('(prefers-color-scheme:dark)').matches) {
-            setChecked(true)
-        }
-    }, [])
-
 
     return (
         <div className="dark-mode">
