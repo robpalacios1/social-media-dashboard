9821376551beeb2aaf748be7fb95294602437d33
roberto <robpalacios1@hotmail.com>
2020-09-01 00:04:39 -0500

    add all component to render App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Global.css';
 import Header from './Header'
 import TopCardList from './Top-card-list'
@@ -7,14 +7,35 @@ import Switch from './Switch'
 
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false)
+  const [checked, setChecked] = useState(false)
+  const mainClass = darkMode ? 'is-dark-mode' : 'is-light-mode'
+
+  function changeMedia(mq){
+    setDarkMode(mq.matches)
+    setChecked(mq.matches)
+}
+
+useEffect(() => {
+    const mq = window.matchMedia('(prefers-color-scheme:dark)')
+    mq.addListener(changeMedia)
+    setDarkMode(mq.matches)
+    setChecked(mq.matches)
+}, [])
+
+
   return (
-    <Fragment>
+    <main className={mainClass}>
       <Header>
-        <Switch />
+        <Switch
+          setDarkMode={setDarkMode}
+          checked={checked}
+          setChecked={setChecked}
+        />
       </Header>
       <TopCardList />
       <Overview />
-    </Fragment>
+    </main>
   );
 }
 
